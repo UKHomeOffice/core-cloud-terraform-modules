7ef4ec932a6bced530f0548c1aec544bf69971d0
Liam MacPherson <liammacpherson1996@gmail.com>
2024-03-14 11:18:40 +0000

    Add support for managed policies in permission sets

--- modules/aws/permission_sets/main.tf
+++ modules/aws/permission_sets/main.tf
@@ -30,3 +30,11 @@ resource "aws_ssoadmin_permission_set_inline_policy" "permission_set_inline_poli
   instance_arn       = var.identity_store_arn
   permission_set_arn = aws_ssoadmin_permission_set.identity_store_permission_set.arn
 }
+
+resource "aws_ssoadmin_managed_policy_attachment" "permission_set_managed_policy" {
+  for_each = toset(var.managed_policies)
+
+  managed_policy_arn = each.value
+  permission_set_arn = aws_ssoadmin_permission_set.identity_store_permission_set.arn
+  instance_arn       = var.identity_store_arn
+}
