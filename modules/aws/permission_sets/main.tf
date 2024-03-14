54c1e8b01cb9d97476dc8aad92b6e33ace297662
Liam MacPherson <liammacpherson1996@gmail.com>
2024-03-14 11:24:07 +0000

    fix: update to just use shortname and prefix specifics

--- modules/aws/permission_sets/main.tf
+++ modules/aws/permission_sets/main.tf
@@ -34,7 +34,7 @@ resource "aws_ssoadmin_permission_set_inline_policy" "permission_set_inline_poli
 resource "aws_ssoadmin_managed_policy_attachment" "permission_set_managed_policy" {
   for_each = toset(var.managed_policies)
 
-  managed_policy_arn = each.value
+  managed_policy_arn = "arn:aws:iam::aws:policy/${each.value}"
   permission_set_arn = aws_ssoadmin_permission_set.identity_store_permission_set.arn
   instance_arn       = var.identity_store_arn
 }
