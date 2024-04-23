29c02049895a24a55d175d95ae828236ed4f3384
Daniel Palmer <daniel.palmer@digital.homeoffice.gov.uk>
2024-04-23 17:01:35 +0100

    fix: remove !

--- modules/aws/users/outputs.tf
+++ modules/aws/users/outputs.tf
@@ -1,3 +1,3 @@
 output "identitystore_user_id" {
-  value = {aws_identitystore_user.identity_center_users.user_name=>aws_identitystore_user.identity_center_users.id}
+  value = {aws_identitystore_user.identity_center_users.user_name = aws_identitystore_user.identity_center_users.id}
 }
