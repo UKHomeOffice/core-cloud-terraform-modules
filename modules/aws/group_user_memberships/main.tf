6fc1e9ec394c1ccd9f58040a2f349a2b0b8ffbb9
Stephen Kaye <stephen.kaye@digital.homeoffice.gov.uk>
2024-04-29 10:30:26 +0100

    Change group_user_memberships to take a group and members by aws id

--- modules/aws/group_user_memberships/main.tf
+++ modules/aws/group_user_memberships/main.tf
@@ -7,34 +7,10 @@ terraform {
   }
 }
 
-data "aws_identitystore_group" "identity_store_groups" {
-  identity_store_id = var.identity_store_id
-  
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "DisplayName"
-      attribute_value = var.group_name
-    }
-  }
-}
-
-data "aws_identitystore_user" "identity_store_users" {
-  for_each = toset(var.users)
-
-  identity_store_id = var.identity_store_id
-
-  alternate_identifier {
-    unique_attribute {
-      attribute_path  = "UserName"
-      attribute_value = each.key
-    }
-  }
-}
-
 resource "aws_identitystore_group_membership" "group_membership" {
-  for_each = toset(var.users)
-  
+  for_each = { for user in var.members : user => user }
+
   identity_store_id = var.identity_store_id
-  group_id = data.aws_identitystore_group.identity_store_groups.id
-  member_id = data.aws_identitystore_user.identity_store_users[each.key].id
+  group_id          = var.group
+  member_id         = each.value
 }
