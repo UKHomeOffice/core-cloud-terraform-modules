d471e1645cbd39f754949900fdfe2c2835d56c32
Stephen Kaye <91200247+stephenkaye-ho@users.noreply.github.com>
2024-04-29 16:46:36 +0100

    Change user module outputs to use user_id (#77)

--- modules/aws/users/outputs.tf
+++ modules/aws/users/outputs.tf
@@ -1,3 +1,3 @@
 output "identitystore_users" {
-  value = { for k, v in aws_identitystore_user.identity_center_users : v.user_name => v.id }
+  value = { for k, v in aws_identitystore_user.identity_center_users : v.user_name => v.user_id }
 }
