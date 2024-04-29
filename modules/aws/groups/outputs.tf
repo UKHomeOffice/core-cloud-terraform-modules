d612243a6b00883dc5b170b207b092401e29fcce
Stephen Kaye <91200247+stephenkaye-ho@users.noreply.github.com>
2024-04-29 14:35:30 +0100

    Update group output to use group_id (#76)

--- modules/aws/groups/outputs.tf
+++ modules/aws/groups/outputs.tf
@@ -1,3 +1,3 @@
 output "identitystore_groups" {
-  value = { for k, v in aws_identitystore_group.identity_store_groups : v.display_name => v.id }
+  value = { for k, v in aws_identitystore_group.identity_store_groups : v.display_name => v.group_id }
 }
