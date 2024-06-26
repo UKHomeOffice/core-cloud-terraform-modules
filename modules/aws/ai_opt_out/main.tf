79a1e9329edf2e1ab4fd715d073d1828ce608b5b
Nicky Barnes <nicky.barnes@digital.homeoffice.gov.uk>
2024-06-26 10:23:48 +0100

    CCL-90 - Adding AI Opt Out Terraform module

---
+++ modules/aws/ai_opt_out/main.tf
@@ -0,0 +1,15 @@
+resource "aws_organizations_policy" "ai_services_opt_out" {
+  name        = var.policy_name
+  description = var.policy_description
+  type        = "AISERVICES_OPT_OUT_POLICY"
+  content     = jsonencode(var.services_overrides)
+}
+
+
+resource "aws_organizations_policy_attachment" "ai_opt_out" {
+  for_each = toset(var.apply_to_ous_or_accounts)
+
+  policy_id = aws_organizations_policy.ai_services_opt_out.id
+  target_id = each.value
+}
+
---
+++ modules/aws/ai_opt_out/variables.tf
@@ -0,0 +1,32 @@
+variable "policy_name" {
+  description = "The name of the AI services opt-out policy"
+  type        = string
+  default     = "AIServicesOptOutPolicy"
+}
+
+variable "policy_description" {
+  description = "The description of the AI services opt-out policy"
+  type        = string
+  default     = "Policy to opt out of AI services"
+}
+
+variable "apply_to_ous_or_accounts" {
+  type        = list(string)
+  description = "A list of AWS Organization OU IDs or AWS Account IDs that should have the Policies applied (can be empty)."
+  default     = []
+}
+
+variable "services_overrides" {
+  description = "Overrides for AI services opt-out policies"
+  default = {
+    "services" : {
+      "@@operators_allowed_for_child_policies" : "@@none"
+      "default" : {
+        "@@operators_allowed_for_child_policies" : "@@none"
+        "opt_out_policy" : {
+          "@@operators_allowed_for_child_policies" : "@@none"
+          "@@assign" : "optOut"
+        }
+      },
+  } }
+}
