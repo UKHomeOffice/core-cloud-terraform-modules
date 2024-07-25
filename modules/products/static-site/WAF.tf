7ac25a88247d1f3ae20b3fa6865213448a08c9e9
Aiden Page <aiden.page@digital.homeoffice.gov.uk>
2024-07-25 11:26:40 +0100

    typo

--- modules/products/static-site/WAF.tf
+++ modules/products/static-site/WAF.tf
@@ -1,7 +1,7 @@
 resource "aws_wafv2_web_acl" "default" {
   name        = "cc-static-site-${var.tags.product}-${var.tags.component}"
   description = "Static Site WAF rule for ${var.tags.product} ${var.tags.component}"
-  scope       = "CLOUDWATCH"
+  scope       = "CLOUDFRONT"
 
   tags = local.common_tags
 
