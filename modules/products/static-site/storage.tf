bb1637663bb4ab27edd8f0d369e161b5b6f00420
Aiden Page <aiden.page@digital.homeoffice.gov.uk>
2024-07-29 10:44:07 +0100

    remove static site hosting config

--- modules/products/static-site/storage.tf
+++ modules/products/static-site/storage.tf
@@ -4,17 +4,17 @@ resource "aws_s3_bucket" "static_site" {
   tags = local.common_tags
 }
 
-resource "aws_s3_bucket_website_configuration" "static_site_config" {
-  bucket = aws_s3_bucket.static_site.id
+# resource "aws_s3_bucket_website_configuration" "static_site_config" {
+#   bucket = aws_s3_bucket.static_site.id
 
-  index_document {
-    suffix = "index.html"
-  }
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
 resource "aws_s3_bucket_public_access_block" "static_site_acl" {
   bucket = aws_s3_bucket.static_site.id
