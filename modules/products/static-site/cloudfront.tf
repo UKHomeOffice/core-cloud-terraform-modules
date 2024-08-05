94e7a80944a7c24ddcfc5c61b880af50ff581cd4
Chris Freeman <98345290+ukho-cfreeman@users.noreply.github.com>
2024-08-05 11:40:45 +0100

    Static site module - disable default caching (#164)

--- modules/products/static-site/cloudfront.tf
+++ modules/products/static-site/cloudfront.tf
@@ -41,7 +41,7 @@ resource "aws_cloudfront_distribution" "static_site_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = 0
     max_ttl                = 86400
   }
 
