29680997ea099f4d599d54a0a69c0f53b29552a8
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2024-09-09 13:27:17 +0100

    CCL-438 updated aws_kms_key with a new name

--- modules/products/static-site/kms.tf
+++ modules/products/static-site/kms.tf
@@ -1,27 +1,27 @@
-resource "aws_kms_key" "static_site_kms" {
+resource "aws_kms_key" "static_site_kms_test" {
   enable_key_rotation = true
   tags                = local.common_tags
 }
 
-resource "aws_kms_key_policy" "static_site_kms_policy" {  
-  key_id = aws_kms_key.static_site_kms.id  
+resource "aws_kms_key_policy" "static_site_kms_policy" {
+  key_id = aws_kms_key.static_site_kms_test.id
   policy = jsonencode({
     Version = "2012-10-17"
-    Id = "static_site_kms_policy"
+    Id      = "static_site_kms_policy"
     Statement = [
       {
-        Sid      = "EnableIAMUserPermissions"
-        Action   = "kms:*"
-        Effect   = "Allow"
+        Sid    = "EnableIAMUserPermissions"
+        Action = "kms:*"
+        Effect = "Allow"
         Principal = {
           AWS = "arn:aws:iam::${local.account_id}:root"
         }
-        Resource = ["*"]     
+        Resource = ["*"]
       },
     ]
     Statement = [
       {
-        Sid      = "CloudFrontServiceKmsPolicy"
+        Sid = "CloudFrontServiceKmsPolicy"
         Action = [
           "kms:Decrypt",
           "kms:Encrypt",
@@ -32,14 +32,14 @@ resource "aws_kms_key_policy" "static_site_kms_policy" {
           type        = "Service"
           identifiers = ["cloudfront.amazonaws.com"]
         }
-        Resource = ["*"]  
+        Resource = ["*"]
         condition = {
-            test     = "StringEquals"
-            variable = "aws:SourceArn"
-            values   = [aws_cloudfront_distribution.static_site_distribution.arn]
-          }
+          test     = "StringEquals"
+          variable = "aws:SourceArn"
+          values   = [aws_cloudfront_distribution.static_site_distribution.arn]
+        }
       },
-    ]    
+    ]
   })
 }
 
