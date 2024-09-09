a8c10a2f68e6d8ef3cfc4f4edb652d1914521b47
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2024-09-09 13:45:22 +0100

    CCL-438 updated kms key

--- modules/products/static-site/storage.tf
+++ modules/products/static-site/storage.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "static_site_encry
   bucket = aws_s3_bucket.static_site.id
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.static_site_kms.arn
+      kms_master_key_id = aws_kms_key.static_site_kms_test.arn
       sse_algorithm     = "aws:kms"
     }
     bucket_key_enabled = true
