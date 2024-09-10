07e30b552ad1e403795cbca93c3a2a4186509107
mynampatinaveen <mynampatinaveen@users.noreply.github.com>
2024-09-10 09:57:51 +0100

    Update kms.tf

--- modules/products/static-site/kms.tf
+++ modules/products/static-site/kms.tf
@@ -4,39 +4,38 @@ resource "aws_kms_key" "static_site_kms" {
 }
 
 resource "aws_kms_key_policy" "static_site_kms_policy" {
-  key_id = aws_kms_key.static_site_kms.id
+key_id = aws_kms_key.static_site_kms.id
   policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "static_site_kms_policy"
-    Statement = [
-      {
-        Sid    = "EnableIAMUserPermissions"
-        Action = "kms:*"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${local.account_id}:root"
+    "Version": "2012-10-17",
+    "Id": "key-default-1",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${local.account_id}:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
         },
-        Resource = ["*"]
-      },
-      {
-        Sid = "CloudFrontServiceKmsPolicy"
-        Action = [
-          "kms:Decrypt",
-          "kms:Encrypt",
-          "kms:GenerateDataKey*"
-        ],
-        Effect = "Allow"
-        Principal = {
-          type        = "Service"
-          identifiers = ["cloudfront.amazonaws.com"]
-        },
-        Resource = ["*"]
-        condition = {
-          test     = "StringEquals"
-          variable = "aws:SourceArn"
-          values   = [aws_cloudfront_distribution.static_site_distribution.arn]
+        {
+            "Sid": "Allow use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudfront.amazonaws.com"
+            },
+            "Action": [
+                "kms:Decrypt",
+                "kms:Encrypt",
+                "kms:GenerateDataKey*"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:SourceArn": "aws_cloudfront_distribution.static_site_distribution.arn"
+                }
+            }
         }
-      },
-    ]  
-   })
+    ]
+})
 }
