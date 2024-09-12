bbfdf675e54ef397d75c80a7073638ed3a829e8f
naveen <naveen.mynampati@digital.homeoffice.gov.uk>
2024-09-12 12:06:45 +0100

    CCL-686 added name to aws_iam_policy

--- modules/products/static-site/kms.tf
+++ modules/products/static-site/kms.tf
@@ -5,44 +5,44 @@ resource "aws_kms_key" "static_site_kms" {
 
 
 resource "aws_kms_key_policy" "static_site_kms_policy" {
-key_id = aws_kms_key.static_site_kms.id
+  key_id = aws_kms_key.static_site_kms.id
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "static_site_kms_policy",
-    "Statement": [
-        {
-            "Sid": "EnableIAMUserPermissions",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "arn:aws:iam::${local.account_id}:root"
-            },
-            "Action": "kms:*",
-            "Resource": "*"
+    "Version" : "2012-10-17",
+    "Id" : "static_site_kms_policy",
+    "Statement" : [
+      {
+        "Sid" : "EnableIAMUserPermissions",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${local.account_id}:root"
         },
-        {
-            "Sid": "CloudFrontServiceKmsPolicyKey",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudfront.amazonaws.com"
-            },
-            "Action": [
-                "kms:Decrypt",
-                "kms:Encrypt",
-                "kms:GenerateDataKey*"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "aws:SourceArn": "aws_cloudfront_distribution.static_site_distribution.arn"
-                }
-            }
+        "Action" : "kms:*",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "CloudFrontServiceKmsPolicyKey",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey*"
+        ],
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:SourceArn" : "aws_cloudfront_distribution.static_site_distribution.arn"
+          }
         }
+      }
     ]
-})
+  })
 }
 
 resource "aws_kms_alias" "static_site_kms_alias" {
-  name        = "alias/static_site/${aws_s3_bucket.static_site.id}"
+  name          = "alias/static_site/${aws_s3_bucket.static_site.id}"
   target_key_id = aws_kms_key.static_site_kms.key_id
 }
 
