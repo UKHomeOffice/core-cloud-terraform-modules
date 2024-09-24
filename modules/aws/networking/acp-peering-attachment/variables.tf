4b830de62eb13410d10b6e982bea78a7a65f5d46
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2024-09-24 11:13:53 +0100

    add networking modules

---
+++ modules/aws/networking/acp-peering-attachment/variables.tf
@@ -0,0 +1,15 @@
+variable "acp_account_id" {
+  default = ""
+}
+
+variable "acp_region" {
+  default = "eu-west-2"
+}
+
+variable "acp_tgw_id" {
+  default = ""
+}
+
+variable "cc_hub_tgw_id" {
+  default = ""
+}
