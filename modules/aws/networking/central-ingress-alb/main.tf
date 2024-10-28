08f3b196b8fe7cd02addc755929eda7693bb7d96
Venkata Para <venkata.para@digital.homeoffice.gov.uk>
2024-10-28 13:50:05 +0000

    adding central-ingress modules for alb and nlb

---
+++ modules/aws/networking/central-ingress-alb/main.tf
@@ -0,0 +1,65 @@
+resource "aws_lb" "lb" {
+  name               = var.name
+  internal           = var.load_balancer_internal
+  load_balancer_type = var.load_balancer_type
+  subnets            = var.subnets
+  security_groups    = [aws_security_group.sg.id]
+  enable_cross_zone_load_balancing = true
+  enable_deletion_protection       = false
+  enable_http2                    = false
+
+#   access_logs {
+#     bucket  = var.access_logs_bucket
+#     enabled = true
+#   }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name     = "${var.prefix}-tg"  # name can't be longer than 32 chars
+  port     = var.tg_port
+  protocol = var.tg_protocol
+  vpc_id   = var.vpc_id
+  target_type = var.target_type
+
+  health_check {
+    protocol            = "HTTPS"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 10
+    interval            = 30
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+
+# Target Group Attachment for IP targets
+resource "aws_lb_target_group_attachment" "lb_target_group_attachment" {
+  for_each = var.target_type == "ip" ? var.nlb_ips : {}
+
+  target_group_arn = aws_lb_target_group.lb_target_group.arn
+  target_id        = each.key               # each.key is the IP address
+  port             = var.tg_port
+  availability_zone = each.value            # each.value is the availability zone
+}
+
