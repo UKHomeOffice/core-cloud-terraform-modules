82ec86f0e9434d4fad878fedba5723f250c49230
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2025-02-05 14:13:41 +0000

    Add ability to specifiy vault policy.

--- modules/aws/backup_vault/main.tf
+++ modules/aws/backup_vault/main.tf
@@ -2,3 +2,10 @@ resource "aws_backup_vault" "this" {
   name = var.name
   tags = var.tags
 }
+
+# Created if the policy JSON is supplied.
+resource "aws_backup_vault_policy" "this" {
+  count             = var.backup_vault_policy_json != "" ? 1 : 0
+  backup_vault_name = aws_backup_vault.this.name
+  policy            = var.backup_vault_policy_json
+}
--- modules/aws/backup_vault/variables.tf
+++ modules/aws/backup_vault/variables.tf
@@ -3,6 +3,12 @@ variable "name" {
   type        = string
 }
 
+variable "backup_vault_policy_json" {
+  description = "Resource Policy JSON for the AWS Backup vault."
+  type        = string
+  default     = ""
+}
+
 variable "tags" {
   description = "Resource tags"
   type        = map(string)
