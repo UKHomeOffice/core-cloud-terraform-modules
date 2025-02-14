2a5f0a3d6d47155b690708b86147304d62219f31
Samiwel Thomas <113512609+samiwelthomasHO@users.noreply.github.com>
2025-02-14 12:56:59 +0000

    Add type info to variable. (#208)
    * Add type info to variable.
    
    * Fix formatting.

--- modules/aws/networking/ctn-connect-attachment/variables.tf
+++ modules/aws/networking/ctn-connect-attachment/variables.tf
@@ -7,10 +7,16 @@ variable "cc_hub_tgw_id" {
 }
 
 variable "tgw_connect" {
+  type = object({
+    ctn_bgp_asn  = string,
+    gateway_cidr = string,
+    gre_cidr     = string,
+    outside_ctn  = list(string)
+  })
   default = {
-    ctn_bgp_asn = "65086"
-    gateway_cidr = "10.238.159.0/24" # Transit Gateway CIDR. Allocated. Minimum /24
-    gre_cidr = "169.254.140.0/28" # BGP CIDR Block 169.254.0.0/16
+    ctn_bgp_asn  = "65086"
+    gateway_cidr = "10.238.159.0/24"  # Transit Gateway CIDR. Allocated. Minimum /24
+    gre_cidr     = "169.254.140.0/28" # BGP CIDR Block 169.254.0.0/16
     outside_ctn = [
       "10.238.0.34", # W2A Hub
       "10.238.0.94"  # W2B Hub
