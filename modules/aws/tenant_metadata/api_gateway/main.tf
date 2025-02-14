4c65e0e15a104389cee52b5e2f52acc19a27e74a
Vijay Jadhav <mail2vjadhav@gmail.com>
2025-02-14 10:43:22 +0000

    CCL-1531: Allow api-gateway to write to cwlogs (#207)

--- modules/aws/tenant_metadata/api_gateway/main.tf
+++ modules/aws/tenant_metadata/api_gateway/main.tf
@@ -1,5 +1,3 @@
-
-
 resource "aws_api_gateway_rest_api" "api" {
   name        = "dynamodb-api"
   description = "API Gateway to interact with DynamoDB"
@@ -11,34 +9,129 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+### POST Method - Insert Tenant ###
+resource "aws_api_gateway_method" "post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "dynamodb" {
+resource "aws_api_gateway_integration" "post_dynamodb" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.proxy.http_method
+  http_method             = aws_api_gateway_method.post.http_method
   integration_http_method = "POST"
-
-  type = "AWS"
-  uri  = "arn:aws:apigateway:us-east-1:dynamodb:action/PutItem"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:dynamodb:action/PutItem"
 
   credentials = var.api_gateway_role_arn
 
   request_templates = {
-    "application/json" = jsonencode({
-      TableName = var.dynamodb_table_name
-      Item = {
-        for key, value in var.attributes_map : key => { S = "$input.path('$.${value}')" }
+    "application/json" = <<EOF
+    {
+      "TableName": "${var.dynamodb_table_name}",
+      "Item": {
+        "tenant": {"S": "$input.path('$.tenant')"}
       }
-    })
+    }
+    EOF
   }
 }
 
+resource "aws_api_gateway_method_response" "post_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "post_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = aws_api_gateway_method_response.post_response.status_code
+
+  response_templates = {
+    "application/json" = <<EOF
+    {
+      "message": "Item inserted successfully",
+      "statusCode": 200
+    }
+    EOF
+  }
+}
+
+### GET Method - Query Tenant ###
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_dynamodb" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:dynamodb:action/Query"
+
+  credentials = var.api_gateway_role_arn
+
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "TableName": "${var.dynamodb_table_name}",
+      "KeyConditionExpression": "tenant = :tenant",
+      "ExpressionAttributeValues": {
+        ":tenant": {"S": "$input.params('tenant')"}
+      }
+    }
+    EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "get_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = aws_api_gateway_method_response.get_response.status_code
+
+  response_templates = {
+    "application/json" = <<EOF
+    #set($inputRoot = $input.path('$'))
+    {
+      "statusCode": 200,
+      "tenants": [
+        #foreach($elem in $inputRoot.Items)
+        {
+          "tenant": "$elem.tenant.S"
+        }#if($foreach.hasNext),#end
+        #end
+      ]
+    }
+    EOF
+  }
+}
+
+### Deploy API Gateway ###
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
@@ -49,12 +142,19 @@ resource "aws_api_gateway_deployment" "deployment" {
   lifecycle {
     create_before_destroy = true
   }
+
+  depends_on = [
+    aws_api_gateway_method.post,
+    aws_api_gateway_method.get,
+    aws_api_gateway_integration.post_dynamodb,
+    aws_api_gateway_integration.get_dynamodb
+  ]
 }
 
-
+### Logging ###
 resource "aws_cloudwatch_log_group" "api_gateway_logs" {
-  name = "/aws/apigateway/access-logs"
-  retention_in_days = 7  # Adjust the retention as per your requirements
+  name              = "/aws/apigateway/access-logs"
+  retention_in_days = 7
 }
 
 resource "aws_api_gateway_stage" "prod" {
@@ -65,12 +165,11 @@ resource "aws_api_gateway_stage" "prod" {
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway_logs.arn
     format          = jsonencode({
-      requestId = "$context.requestId"
-      sourceIp = "$context.identity.sourceIp"
-      userAgent = "$context.identity.userAgent"
+      requestId  = "$context.requestId"
+      sourceIp   = "$context.identity.sourceIp"
+      userAgent  = "$context.identity.userAgent"
       requestTime = "$context.requestTime"
-      status = "$context.status"
+      status     = "$context.status"
     })
   }
 }
-
--- modules/aws/tenant_metadata/iam/main.tf
+++ modules/aws/tenant_metadata/iam/main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_policy" "api_gateway_dynamodb_policy" {
     Version = "2012-10-17"
     Statement = [{
       Effect   = "Allow"
-      Action   = ["dynamodb:PutItem"]
+      Action   = ["dynamodb:Query", "dynamodb:PutItem"]
       Resource = [
         "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.dynamodb_table_name}",
         "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.dynamodb_table_name}/*"
@@ -38,4 +38,43 @@ resource "aws_iam_role_policy_attachment" "attach_api_gateway_policy" {
 
 data "aws_caller_identity" "current" {}
 
+
+
+resource "aws_iam_policy" "api_gateway_cloudwatch_policy" {
+  name        = "api-gateway-cloudwatch-policy"
+  description = "Allows API Gateway to write logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/apigateway/access-logs"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_api_gateway_cloudwatch_policy" {
+  policy_arn = aws_iam_policy.api_gateway_cloudwatch_policy.arn
+  role       = aws_iam_role.api_gateway_role.name
+}
+
+# TO BE DONE MANUALLY - ONCE - AS THIS WONT WORK
+
+# resource "null_resource" "set_apigateway_account" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       aws apigateway update-account \
+#       --patch-operations op="replace",path="/cloudwatchRoleArn",value="${aws_iam_role.api_gateway_role.arn}"
+#     EOT
+#   }
+
+#   depends_on = [aws_iam_role_policy_attachment.attach_api_gateway_cloudwatch_policy]
+# }
 
