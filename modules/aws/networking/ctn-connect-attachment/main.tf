9fdb0165b74dd099a2423635f748f3ba29e6d922
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2025-02-14 12:56:10 +0000

    Fix formatting.

--- modules/aws/networking/ctn-connect-attachment/main.tf
+++ modules/aws/networking/ctn-connect-attachment/main.tf
@@ -5,10 +5,10 @@ resource "aws_ec2_transit_gateway_connect" "connect" {
 }
 
 resource "aws_ec2_transit_gateway_connect_peer" "w2-tx01" {
-  for_each = { "a" : 0, "b" : 1 }
+  for_each                      = { "a" : 0, "b" : 1 }
   peer_address                  = var.tgw_connect["outside_ctn"][each.value]
-  transit_gateway_address = cidrhost(var.tgw_connect["gateway_cidr"], each.value+1)
-  inside_cidr_blocks = [cidrsubnet(var.tgw_connect["gre_cidr"], 1, each.value)]
+  transit_gateway_address       = cidrhost(var.tgw_connect["gateway_cidr"], each.value + 1)
+  inside_cidr_blocks            = [cidrsubnet(var.tgw_connect["gre_cidr"], 1, each.value)]
   bgp_asn                       = var.tgw_connect["ctn_bgp_asn"]
   transit_gateway_attachment_id = aws_ec2_transit_gateway_connect.connect.id
 }
@@ -16,5 +16,5 @@ resource "aws_ec2_transit_gateway_connect_peer" "w2-tx01" {
 resource "aws_ec2_transit_gateway_route" "blackhole-route-table-entry" {
   destination_cidr_block         = var.cc_destination_cidr_block
   transit_gateway_route_table_id = var.cc_transit_gateway_route_table_id
-  transit_gateway_attachment_id = var.cc_transit_gateway_attachment_id # CC Prod TGW attachment
+  transit_gateway_attachment_id  = var.cc_transit_gateway_attachment_id # CC Prod TGW attachment
 }
