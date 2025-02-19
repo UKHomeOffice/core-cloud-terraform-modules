92df25fd161f09195655f4755d1e63f7affeb54f
Shabazz Abdulrahman <Shabazz.Abdulrahman@digital.homeoffice.gov.uk>
2025-02-19 11:57:25 +0000

    adding tgw-vpc-attachment module

---
+++ modules/aws/networking/tgw-vpc-attachment/variables.tf
@@ -0,0 +1,32 @@
+variable "transit_gateway_id" {
+  description = "The ID of the Transit Gateway."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to attach."
+  type        = string
+}
+
+variable "azs" {
+  description = "List of Availability Zones for which the dedicated /28 subnets exist. The order of the AZs determines the order of subnets used in the attachment."
+  type        = list(string)
+  # Example: ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+variable "attachment_subnet_ids" {
+  description = "A map of dedicated /28 subnet IDs for each AZ. The keys must match the AZ names provided in `azs`."
+  type        = map(string)
+  # Example:
+  # {
+  #   "eu-west-2a" = "cc-vpn-prod-tgwattach-a",
+  #   "eu-west-2b" = "cc-vpn-prod-tgwattach-b",
+  #   "eu-west-2c" = "cc-vpn-prod-tgwattach-c"
+  # }
+}
+
+variable "appliance_mode_support" {
+  description = "Enable appliance mode support if required."
+  type        = bool
+  default     = false
+}
