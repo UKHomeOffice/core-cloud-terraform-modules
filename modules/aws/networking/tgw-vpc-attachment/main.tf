a23a6e2d66454dda4f5a04a475d45cff8f4a29ae
Shabazz Abdulrahman <Shabazz.Abdulrahman@digital.homeoffice.gov.uk>
2025-02-20 16:16:34 +0000

    adding gateway prpagtion option in module

--- modules/aws/networking/tgw-vpc-attachment/main.tf
+++ modules/aws/networking/tgw-vpc-attachment/main.tf
@@ -7,8 +7,9 @@ locals {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "twg_vpc" {
-  transit_gateway_id = var.transit_gateway_id
-  vpc_id             = var.vpc_id
-  subnet_ids         = local.ordered_attachment_subnet_ids
+  transit_gateway_id                              = var.transit_gateway_id
+  vpc_id                                          = var.vpc_id
+  subnet_ids                                      = local.ordered_attachment_subnet_ids
+  transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
 
 }
