3cec3e864f2ea152d282f05d08a2bfbcee30d3b4
jakebarkerHO <acob.barker2@homeoffice.gov.uk>
2025-04-14 12:53:46 +0100

    added delete protection to network firewall

--- modules/aws/networking/network-firewall-rules-egress/main.tf
+++ modules/aws/networking/network-firewall-rules-egress/main.tf
@@ -18,6 +18,7 @@ resource "aws_networkfirewall_firewall" "existing_firewall" {
   name                = var.network_firewall_name ## Existing firewall name
   vpc_id              = var.vpc_id                ## Use the existing VPC ID
   firewall_policy_arn = aws_networkfirewall_firewall_policy.policy.arn
+  delete_protection   = true
 
   # Subnet mappings (use the existing subnets here)
   dynamic "subnet_mapping" {
