d1a0fd56d3f9a9d3550a74f79733fae25e33aab8
jakebarkerHO <acob.barker2@homeoffice.gov.uk>
2025-04-14 12:12:23 +0100

    rm tfsec skip for lambda, no longer required

--- modules/aws/logging/cwl_subscribe_with_tags/lambda.tf
+++ modules/aws/logging/cwl_subscribe_with_tags/lambda.tf
@@ -100,7 +100,6 @@ data "aws_iam_policy_document" "lambda" {
       "logs:PutLogEvents"
     ]
 
-    # tfsec:ignore:aws-iam-no-policy-wildcards
     resources = [
       "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
     ]
