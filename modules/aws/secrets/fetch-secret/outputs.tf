152382689e6a76aa35dc1e01a492be88034872d7
Juha Remes <juha.remes@digital.homeoffice.gov.uk>
2025-04-22 13:19:55 +0100

    Add Slack webhook as output in fetch-secret

--- modules/aws/secrets/fetch-secret/outputs.tf
+++ modules/aws/secrets/fetch-secret/outputs.tf
@@ -2,3 +2,8 @@ output "hec_token" {
   value     = local.secret_data["splunk-hec-token"]
   sensitive = true
 }
+
+output "security_hub_alert_webhook" {
+  value     = local.secret_data["webhook"]
+  sensitive = true
+}
