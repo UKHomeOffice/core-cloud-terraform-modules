60e57078988309bef7e8ababa40e59c0569be2f4
Venkata Para <venkata.para@digital.homeoffice.gov.uk>
2025-04-29 09:58:07 +0100

    updating the inpsectorv2 module

--- modules/aws/inspectorv2/main.tf
+++ modules/aws/inspectorv2/main.tf
@@ -3,19 +3,29 @@ provider "aws" {
   region = var.region
 }
 
+# Determining the audit_account_id based on current running env
+data "aws_caller_identity" "current" {}
+
+locals {
+  current_mgmt_id   = data.aws_caller_identity.current.account_id
+
+  audit_account_id = local.current_mgmt_id == var.dev_lza_mgmt_id ? var.dev_audit_account_id : var.prod_audit_account_id
+}
+
 # Enable Inspector service & register Audit account as delegated admin (via CLI)
 resource "null_resource" "org_prereqs" {
   provisioner "local-exec" {
     command = <<EOT
       aws organizations enable-aws-service-access --service-principal inspector2.amazonaws.com --region ${var.region}
-      aws organizations register-delegated-administrator --account-id ${var.audit_account_id} --service-principal inspector2.amazonaws.com --region ${var.region}
+      aws organizations register-delegated-administrator --account-id ${local.audit_account_id} --service-principal inspector2.amazonaws.com --region ${var.region}
     EOT
   }
 }
 
 # Register Audit account as delegated Inspector admin
 resource "aws_inspector2_delegated_admin_account" "this" {
-  account_id = var.audit_account_id
+  provider   = aws.management
+  account_id = local.audit_account_id
 
   depends_on = [null_resource.org_prereqs]
 }
@@ -24,31 +34,15 @@ resource "aws_inspector2_delegated_admin_account" "this" {
 resource "aws_inspector2_enabler" "enable_in_audit_account" {
   provider = aws.management
 
-  account_ids    = [var.audit_account_id]
+  account_ids    = [local.audit_account_id]
   resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
+
+  lifecycle {
+    ignore_changes = [
+      id
+    ]
+    create_before_destroy = true
+  }
 
   depends_on = [aws_inspector2_delegated_admin_account.this]
-}
-
-# Get all accounts in the AWS Organization
-data "aws_organizations_organization" "this" {
-  provider = aws.management
-}
-
-# Create list of active member accounts (excluding the management account)
-locals {
-  member_account_ids = [
-    for acct in data.aws_organizations_organization.this.accounts :
-    acct.id if acct.status == "ACTIVE" && acct.id != data.aws_organizations_organization.this.master_account_id
-  ]
-}
-
-# Enable Inspector in all active member accounts
-resource "aws_inspector2_enabler" "enable_in_member_accounts" {
-  provider = aws.management
-
-  account_ids    = local.member_account_ids
-  resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
-
-  depends_on = [aws_inspector2_enabler.enable_in_audit_account]
 }
--- modules/aws/inspectorv2/outputs.tf
+++ modules/aws/inspectorv2/outputs.tf
@@ -2,7 +2,3 @@ output "delegated_admin_account_id" {
   value = aws_inspector2_delegated_admin_account.this.account_id
 }
 
-output "enabled_member_accounts" {
-  value = local.member_account_ids
-}
-
