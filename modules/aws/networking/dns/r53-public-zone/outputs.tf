4f7d6dd9059deb86b83270209209976ee3bb395a
Ed Bent <edward.bent@digital.homeoffice.gov.uk>
2025-06-18 14:35:25 +0100

    removed DNS modules

--- modules/aws/networking/dns/r53-public-zone/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_id" {
-  description = "The ID of the Route 53 hosted zone"
-  value       = aws_route53_zone.workload_zone.zone_id
-}
-
-output "name_servers" {
-  description = "List of name servers for delegation"
-  value       = aws_route53_zone.workload_zone.name_servers
-}
--- modules/aws/networking/dns/zone-delegation/main.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_route53_record" "ns_record" {
-  zone_id = var.zone_id
-  name    = var.domain_name
-  type    = "NS"
-  ttl     = 300
-
-  records = var.workload_public_zone_ns_records
-}
