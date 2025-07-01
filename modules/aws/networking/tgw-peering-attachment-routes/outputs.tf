1eabe4354582cc3172679d38a7c7cb9825fdcb2c
Vijay Jadhav <mail2vjadhav@gmail.com>
2025-07-01 09:03:33 +0100

    Central TGW HUB Routes to prod and notprod

--- modules/aws/networking/tgw-peering-attachment-routes/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "tgw_routes_created" {
-  description = "Map of created TGW route resource IDs keyed by CIDR"
-  value = {
-    for cidr, res in aws_ec2_transit_gateway_route.to_remote_cidrs :
-    cidr => res.id
-  }
-}
