79998c338f9f374b821ca0e285f65a8f30f18854
Iqbal.Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2026-02-25 16:06:50 +0000

    CCL-7111 - updating deprecated name with region

--- modules/aws/logging/cwl_subscribe_with_tags/lambda.tf
+++ modules/aws/logging/cwl_subscribe_with_tags/lambda.tf
@@ -105,7 +105,7 @@ data "aws_iam_policy_document" "lambda" {
 
     # tfsec:ignore:aws-iam-no-policy-wildcards
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"
+      "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*"
     ]
   }
 }
